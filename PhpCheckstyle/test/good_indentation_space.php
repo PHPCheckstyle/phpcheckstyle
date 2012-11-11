56dfd525c910db3d9bb0e8f22db0ecc5db5dd9c6
tchule <tchule@hotmail.com>
2012-11-11 10:52:20 +0000

    Issue 39:	Error on closing tag for space-based indentation checks

---
+++ PhpCheckstyle/test/good_indentation_space.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Test 4 spaces indentation.
+ */
+class Indentation {
+
+    /**
+     * Test.
+     */
+    function foo() {
+       
+        $a = 0;
+
+        //code
+        if ($a == 1) {
+            // new code
+            echo "toto";
+        }
+
+    } 
+
+}
---
+++ PhpCheckstyle/test/todo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * TODO : The todo message.
+ *
+ * @param String $var a var
+ */
+function toto($a) {
+	$a = 1; // this is line 5, but the warning is for line 4
+}

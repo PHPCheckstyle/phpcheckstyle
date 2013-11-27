53f01ad2d1434deab93dc1507c517ef1dbbb9dfc
tchule@hotmail.com <tchule@hotmail.com@9aec88b1-1b24-c516-cc61-d7528b567548>
2013-11-27 10:37:49 +0000

    Réorganisation dans SVN

--- trunk/PhpCheckstyle/phpcheckstyle.cmd
+++
@@ -1,3 +0,0 @@
-echo "PHP Checkstyle script"
-php -c C:\ms4w\Apache\cgi-bin\php.ini run.php --src ./test/issue53.php --outdir ./checkstyle_result --config default.cfg.xml --format html,xml --linecount --debug
-pause
--- trunk/PhpCheckstyle/test/good_line_lenght.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Some php code here
-?>
-                                                                                                                                                                                              A VERY LONG LINE
-                                                                                                                                                                                              AGAIN
-<?php
-// More code here

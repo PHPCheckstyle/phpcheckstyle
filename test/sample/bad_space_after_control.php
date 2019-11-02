ab615f9cd3861a868188598b9a836909f4e62d55
Charlotte Dunois <github@charuru.moe>
2019-11-02 22:32:07 +0100

    Add no space after control stmt test option

---
+++ test/sample/bad_space_after_control.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is an exemple of PHP file containing bad spacing around control statement.
+ * This file should generate 1 warning.
+ *
+ * @SuppressWarnings localScopeVariableLength
+ */
+class Space {
+
+	/**
+	 * Test spacing
+	 *
+	 * @param String $a
+	 * @param String $b
+	 * @return String
+	 */
+	function testSpaces($a, $b) {
+		if ($a === null) { // 1 - noSpaceAfterControlStmt (if statement)
+			return $b;
+		}
+	}
+
+}
